0e69becb568483eeae3c4e6b5927fc50f7f28dd6
Infabls <infabls.com@gmail.com>
2018-09-19 21:30:54 +0600

    order on email
    create func mail

--- section/call_q.php
+++ section/call_q.php
@@ -19,26 +19,11 @@
 <div class="modal-header"><button class="close" type="button" data-dismiss="modal">×</button>
 <h3 class="modal-title">Оставьте заявку</h3>
 </div>
-<form action="" method="POST">
+<form action="/func/mail.php" method="POST">
 <div class="modal-body">
 	<div class="modal-inp"><input name="name" id="modal-inp" placeholder="Как к вам обращаться?" class="dushbord__form_input" type="text"></div>
 	<div class="modal-tel"><input name="phone" type="tel" id="modal-inp" placeholder="Укажите свой телефон" class="dushbord__form_input" type="text"></div>
 		<div class="modal-tel"><input name="email" type="email" id="modal-inp" placeholder="E-mail (не обязательно)" class="dushbord__form_input" type="text"></div>
-		<?php
-			if(!empty($_POST['phone'])){
-	//Получаем данные из глобальной переменной $_GET, так как мы передаем данные методом GET
-	$name = $_POST['name']; // Вытаскиваем имя в переменную
-	//$email = $_POST['e-mail']; // Вытаскиваем почту в переменную
-	$phone= $_POST['phone'];
-	$email = $_POST['email'];
-	$message = "Имя клиента: $name, <br> Номер телефона: $phone <br> Почта: $email"; // Формируем сообщение, отправляемое на почту
-	$to = 'gorgaz2050@mail.ru'; // Задаем получателя письма
-	$from = $phone;  // От кого пришло письмо
-	$subject = "Заявка c сайта. Подвальная форма обратной связи"; // Задаем тему письма
-	$headers = "От кого: $from\r\nReply-To: $to\r\nContent-type: text/html; charset=utf-8\r\n"; // Формируем заголовок письма (при неправильном формировании может ломаться кодировка и т.д.)
-	mail($to, $subject, $message, $headers);
-		}
-?>
 	<div class="modal-name"><input id="modal-btn" type="submit" name="submit" value="ОТПРАВИТЬ" class="dushbord__form_submit"" type="submit"></div>
 
 	<div class="modal-txt"><p style="color:#fff;">Мы свяжемся с вами в ближайшее время!</p></div>
